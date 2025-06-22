4cc0b97ea20649d82d3fbcc757778b5a8c30ec7d
Kevin <vladyslav.dubinskyy@hotmail.com>
2025-06-22 20:01:34 +0200

    Remove unused BRAND_CHANNEL constant

--- src/main/java/ua/nanit/limbo/LimboConstants.java
+++ src/main/java/ua/nanit/limbo/LimboConstants.java
@@ -20,7 +20,6 @@ package ua.nanit.limbo;
 public final class LimboConstants {
 
     public static final String VELOCITY_INFO_CHANNEL = "velocity:player_info";
-    public static final String BRAND_CHANNEL = "minecraft:brand";
 
     private LimboConstants() {}
 
